2cb172fd9c72af508facbccfcf30a439ef732a87
Diego Viniegra <diego.viniegra@gmail.com>
2020-04-30 17:22:46 -0500

    WIP: sorting done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Filters from './components/Filters';
 import Showcase from './components/Showcase';
 import SearchBar from './components/SearchBar';
@@ -9,6 +10,8 @@ import SortFilters from './components/SortFilters';
 class App extends Component {
   state = {
     originalData: [],
+
+    currentSorting: '',
 
     customFilters: [
       { name: 'Todo', value: 'all', checked: true },
@@ -35,28 +38,205 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.dataService = props.dataService;
+    this.cartService = props.cartService;
+
     this.onURLChange = this.onURLChange.bind(this)
     this.onFilterChange = this.onFilterChange.bind(this)
-    this.sortProducts = this.sortProducts.bind(this)
     this.onSearchChange = this.onSearchChange.bind(this)
+    this.updateProducts = this.updateProducts.bind(this)
+    this.sortProducts = this.sortProducts.bind(this)
   }
 
-  onURLChange() {
-
+  componentDidMount() {
+    this.dataService.getData().then(data => {
+      this.setState({
+        originalData: data,
+        mainFilter: {
+          search: '',
+          categories: data.categories.slice(0),
+          customFilter: this.state.customFilters[0],
+          priceFilter: this.state.priceFilters[0]
+        },
+        // Make a deep copy of the original data to keep it immutable
+        products: data.products.slice(0)
+      });
+    });
   }
 
-  onFilterChange() {
+  componentDidUpdate(_, prevState) {
+    const { products } = this.state;
 
+    if (prevState.products.length !== products.length) {
+      this.sortProducts('name');
+    }
   }
 
-  sortProducts() {
+  //////////// Methods ///////////////////////
 
+  onURLChange(url) {
+    this.dataService.getRemoteData(url).subscribe(data => {
+      this.setState({
+        originalData: data,
+        mainFilter: {
+          search: '',
+          categories: data.categories.slice(0),
+          customFilter: this.customFilters[0],
+          priceFilter: this.priceFilters[0]
+        },
+        products: data.products.slice(0)
+      });
+      // this clean SearchBar when url change, is pending
+      // this.searchComponent.reset();
+
+      // this.cartService.flushCart();
+    });
   }
 
-  onSearchChange() {
+  onSearchChange(search) {
+    this.setState({
+      mainFilter: { ...this.state.mainFilter, search: search.search }
+    });
 
+    this.updateProducts({
+      type: 'search',
+      change: search.change
+    });
   }
 
+  onFilterChange(data) {
+    if (data.type === 'category') {
+      if (data.isChecked) {
+        this.mainFilter.categories.push(data.filter);
+      } else {
+        this.mainFilter.categories = this.mainFilter.categories.filter(
+          category => {
+            return category.categori_id !== data.filter.categori_id;
+          });
+      }
+    } else if (data.type === 'custom') {
+      this.mainFilter.customFilter = data.filter;
+    } else if (data.type === 'price') {
+      this.mainFilter.priceFilter = data.filter;
+    }
+    this.updateProducts({
+      type: data.type,
+      change: data.change
+    });
+  }
+
+  updateProducts(filter) {
+    let productsSource = this.originalData.products;
+    const prevProducts = this.products;
+    let filterAllData = true;
+    if ((filter.type === 'search' && filter.change === 1) || (filter.type === 'category' && filter.change === -1)) {
+      productsSource = this.products;
+      filterAllData = false;
+    }
+    // console.log('filtering ' + productsSource.length + ' products')
+
+    this.products = productsSource.filter(product => {
+      // Filter by search
+      if (filterAllData || filter.type === 'search') {
+        if (!product.name.match(new RegExp(this.mainFilter.search, 'i'))) {
+          return false;
+        }
+      }
+
+      // Filter by categories
+      if (filterAllData || filter.type === 'category') {
+        let passCategoryFilter = false;
+        product.categories.forEach(product_category => {
+          if (!passCategoryFilter) {
+            passCategoryFilter = this.mainFilter.categories.reduce((found, category) => {
+                return found || product_category === category.categori_id;
+            }, false);
+          }
+        });
+        if (!passCategoryFilter) {
+          return false;
+        }
+      }
+
+      // Filter by custom filters
+      if (filterAllData || filter.type === 'custom') {
+        let passCustomFilter = false;
+        const customFilter = this.mainFilter.customFilter.value;
+        if (customFilter === 'all') {
+          passCustomFilter = true;
+        } else if (customFilter === 'available' && product.available) {
+          passCustomFilter = true;
+        } else if (customFilter === 'unavailable' && !product.available) {
+          passCustomFilter = true;
+        } else if (customFilter === 'bestseller' && product.best_seller) {
+          passCustomFilter = true;
+        }
+        if (!passCustomFilter) {
+          return false;
+        }
+      }
+
+      // Filter by price filters
+      if (filterAllData || filter.type === 'price') {
+        let passPriceFilter = false;
+        const customFilter = this.mainFilter.priceFilter.value;
+        const productPrice = parseFloat(product.price.replace(/\./g, '').replace(',', '.'));
+        if (customFilter === 'all') {
+          passPriceFilter = true;
+        } else if (customFilter === 'more_30000' && productPrice > 30000) {
+          passPriceFilter = true;
+        } else if (customFilter === 'less_10000' && productPrice < 10000) {
+          passPriceFilter = true;
+        }
+        if (!passPriceFilter) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+
+    // If the number of products increased after the filter has been applied then sort again
+    // If the number of products remained equal, there's a high chance that the items have been reordered.
+    if (prevProducts.length <= this.products.length && this.products.length > 1) {
+      this.sortProducts(this.state.currentSorting);
+    }
+
+    // These two types of filters usually add new data to the products showcase so a sort is necessary
+    if (filter.type === 'custom' || filter.type === 'price') {
+      this.sortProducts(this.state.currentSorting);
+    }
+  }
+
+  sortProducts(criteria) {
+    // console.log('sorting ' + this.products.length + ' products')
+    const products = this.state.products.slice()
+      .sort((a, b) => {
+        const priceComparison = parseFloat(a.price.replace(/\./g, '')
+        .replace(',', '.')) - parseFloat(b.price.replace(/\./g, '').replace(',', '.'));
+        if (criteria === 'priceDes') {
+          return -priceComparison;
+        } else if (criteria === 'priceAsc') {
+          return  priceComparison;
+        } else if (criteria === 'name') {
+          const nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        } else {
+          // Keep the same order in case of any unexpected sort criteria
+          return -1;
+        }
+      });
+
+    this.setState({ products, currentSorting: criteria });
+  }
+
+  /////////////// render function /////////////
   render() {
     const {
       originalData,
@@ -64,7 +244,7 @@ class App extends Component {
       priceFilters,
       sortFilters,
       products,
-    } this.state;
+    } = this.state;
 
     return (
       <div className="main-container">
